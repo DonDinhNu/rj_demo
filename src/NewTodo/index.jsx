d99303fd472db9193a9d77d3062ee729cc3ea02a
DonDinh <don.dinh@employmenthero.com>
2017-08-29 12:27:59 +0700

    Apply lodash.

--- src/NewTodo/index.jsx
+++ src/NewTodo/index.jsx
@@ -1,19 +1,58 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {addTagAction} from './state';
+import {removeLatestTag, tagsSelector} from './state';
+
 import './index.css';
 import ListTag from '../ListTag';
 
-const NewTodo = ({tags}) => (
-  <form>
+const NewTodo = ({tags, addTag, removeLatestTag}) => (
+  <div>
+    <h1></h1>
     <label> Title: </label>
     <input type='text' name='title' placeholder='Title please'/>
     <br/>
     <div className='tags'>
       <label> Tags: </label>
       <ListTag tags={tags}/>
-      <input type='text' name='title' placeholder='+ New tag'/>
+      <input type='text' name='title' placeholder='+ New tag' onKeyPress={addTag}/>
     </div>
     <input type='submit' value='Create New Tag'/>
-  </form>
+    <input type='button' value='Remove' onClick={removeLatestTag}/>
+  </div>
 );
 
-export default NewTodo;
+const mapDispatchToProps = dispatch => ({
+  // Addtag is function with data to dispatch
+  addTag:  e => {
+    if (e.charCode === 13) {
+      dispatch(addTagAction(e.target.value));
+      e.target.value = '';
+    }
+  },
+
+  removeLatestTag: () => {
+    dispatch(removeLatestTag())
+  }
+});
+
+const mappStateToProps = state => ({
+  tags: tagsSelector(state),
+}
+);
+
+const enhance = connect(mappStateToProps, mapDispatchToProps);
+
+export default enhance(NewTodo);
+
+//mapDispatchToProps
+//1. Patch action to event
+//2. Add action as param on const NewTodo
+//3. On mapDispatchToProps, define a key with name is action and handle dispatch with action.
+// dispatch action, reducer handle action to update state
+
+
+//mappStateToProps
+// Recieve state
+
+// lodash
--- src/NewTodo/state.js
+++ src/NewTodo/state.js
@@ -1,16 +1,20 @@
-import {handleActions} from 'redux-actions';
+import {createAction, handleActions} from 'redux-actions';
 import {combineReducers} from 'redux';
+import {uniq, path} from 'lodash/fp';
 
 // Define action
 const ADD_TAG = 'NewTodo/addTag';
+const REMOVE_TAG = 'NewTodo/removeTag';
 
-const addTagAction = data  => ({
-  type: ADD_TAG,
-  payload: data,
-})
+export const addTagAction = createAction(ADD_TAG);
+export const tagsSelector = path('newTodo.tags');
+export const removeLatestTag = createAction(REMOVE_TAG);
 
-const tags = handleActions({
-  [ADD_TAG]: (state, action)  => [...state, action.payload],},
+const tags = handleActions(
+  {
+    [ADD_TAG]: (state, action)  =>uniq([...state, action.payload]),
+    [REMOVE_TAG]: (state, _action) => state.slice(0, -1),
+  },
   []
 );
 
